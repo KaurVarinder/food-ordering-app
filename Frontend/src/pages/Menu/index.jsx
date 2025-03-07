c8a49a7b16ce1572e220d8dbfc236fb512e7a85e
Varinder <kaurv298@gmail.com>
2025-03-08 03:58:09 +0530

    first-commit

--- Frontend/src/pages/Menu/index.jsx
+++ Frontend/src/pages/Menu/index.jsx
@@ -1,66 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchProducts, selectAllProducts } from "../../stores/menu/productSlice";
-// import ProductDetailCard from "../../components/ProductDetailCard";
-// import { Tabs } from "../../components/Tabs";
-// import { addToCart } from "../../stores/Cart/cartSlice";
-
-// const Menu = () => {
-//     const dispatch = useDispatch();
-//     const products = useSelector(selectAllProducts);
-
-//     const [activeTab, setActiveTab] = useState('');
-//     const [activeTabIndex, setActiveTabIndex] = useState(0);
-
-//     useEffect(() => {
-//         console.log("Fetching products...");
-//         dispatch(fetchProducts())
-//           .unwrap()
-//           .then((data) => console.log("Fetched products:", data))
-//           .catch((error) => console.log("Error fetching products:", error));
-//     }, [dispatch]);
-    
-
-//     const onAddProduct = (product) => {
-//         dispatch(addToCart(product));
-//     };
-
-//     const onTabSwitch = (newActiveTab) => {
-//         setActiveTab(newActiveTab);
-//         let categories = products.products?.map((product) => product.name?.name);
-//         let index = categories.findIndex(category => newActiveTab === category);
-//         setActiveTabIndex(index > -1 ? index : 0);
-//     };
-
-//     return (
-//         <div className="bg-white">
-//             {products.status !== "fulfilled" ? (
-//                 <div>Loading....</div>
-//             ) : (
-//                 <div className="menu-wrapper">
-//                     {products.products && (
-//                         <Tabs
-//                             list={products.products.map((product) => product.name.name)}
-//                             activeTab={activeTab}
-//                             onTabSwitch={onTabSwitch}
-//                         />
-//                     )}
-//                     <div className="flex flex-row mx-3">
-//                         {products.products &&
-//                             products.products.length > 0 &&
-//                             products.products[activeTabIndex].products &&
-//                             products.products[activeTabIndex].products.map((product, index) => (
-//                                 <ProductDetailCard key={index} product={product} onAddProduct={onAddProduct} />
-//                             ))}
-//                     </div>
-//                 </div>
-//             )}
-//         </div>
-//     );
-// };
-
-// export default Menu;
-
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts, selectAllProducts } from "../../stores/menu/productSlice";
@@ -75,12 +12,11 @@ const Menu = () => {
     const [activeTab, setActiveTab] = useState('');
     const [activeTabIndex, setActiveTabIndex] = useState(0);
 
-    // ✅ Fetch products on mount
     useEffect(() => {
         dispatch(fetchProducts());
     }, [dispatch]);
 
-    console.log("Products state:", products); // ✅ Debugging log
+    console.log("Products state:", products); 
 
     const onAddProduct = (product) => {
         dispatch(addToCart(product));
@@ -88,8 +24,7 @@ const Menu = () => {
 
     const onTabSwitch = (newActiveTab) => {
         setActiveTab(newActiveTab);
-        
-        // ✅ Ensure we extract the correct category names
+
         let categories = products.products.map((category) => category.name?.toUpperCase());
     
         let index = categories.findIndex(category => newActiveTab === category);
