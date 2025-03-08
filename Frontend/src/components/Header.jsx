75866e8a64703dcb1cc9b3432a8612181ba28b60
Varinder <kaurv298@gmail.com>
2025-03-08 20:10:08 +0530

    first-commit

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -37,17 +37,17 @@ export const Header = ({ cartCount }) => {
 
     return (
         <nav id="header" className="bg-black text-white">
-            <div className="w-full container mx-auto flex flex-col md:flex-row items-center justify-between mt-0 py-2">
+            <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
                 <div className="logo-wrapper pl-4 flex items-center">
                     <Link to="/" className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text">
-                        <img src={foody} alt="logo" className="w-24 h-24 md:w-40 md:h-40 object-cover" />
+                        <img src={foody} alt="logo" className="w-40 h-40 object-cover" />
                     </Link>
                 </div>
-                <div className="nav-menu-wrapper flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-10">
+                <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
                     <Link to="/" className="text-xl">Home</Link>
                     <button onClick={scrollToAbout} className="text-xl cursor-pointer">About</button>
                 </div>
-                <div className="flex flex-col md:flex-row items-center justify-center space-y-2 md:space-y-0 md:space-x-4 mx-4">
+                <div className="flex items-center justify-center space-x-4 mx-10">
                     <Link to="/cart" className="mr-4 relative">
                         Cart
                         {cartCount > 0 ? <div className="rounded-full bg-yellow-400 text-white inline-flex justify-center items-center w-full absolute -top-3 -right-6">{cartCount}</div> : null}
@@ -65,5 +65,6 @@ export const Header = ({ cartCount }) => {
                 </div>
             </div>
         </nav>
+
     )
 }
