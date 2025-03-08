4f8ab63557a995982b368c176f0224bbc59416c8
Varinder <kaurv298@gmail.com>
2025-03-08 20:12:00 +0530

    first-commit

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -36,32 +36,33 @@ export const Header = ({ cartCount }) => {
 
 
     return (
-        <nav id="header" className="bg-black text-white">
-            <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
-                <div className="logo-wrapper pl-4 flex items-center">
-                    <Link to="/" className="toggleColor text-white no-underline hover:no-underline font-bold text-2xl lg:text">
-                        <img src={foody} alt="logo" className="w-40 h-40 object-cover" />
-                    </Link>
-                </div>
-                <div className="nav-menu-wrapper flex items-center justify-between space-x-10">
-                    <Link to="/" className="text-xl">Home</Link>
-                    <button onClick={scrollToAbout} className="text-xl cursor-pointer">About</button>
-                </div>
-                <div className="flex items-center justify-center space-x-4 mx-10">
-                    <Link to="/cart" className="mr-4 relative">
+        <nav className="bg-black text-white">
+            <div className="w-full container mx-auto flex items-center justify-between py-2 px-4">
+                <Link to="/" className="flex items-center">
+                    <img src={foody} alt="logo" className="w-24 h-24 md:w-40 md:h-40 object-cover" />
+                </Link>
+
+                <button className="md:hidden text-white text-2xl" onClick={() => setMenuOpen(!menuOpen)}>
+                    ☰
+                </button>
+
+                <div className={`md:flex items-center space-x-6 ${menuOpen ? "block" : "hidden"} absolute md:static top-16 left-0 w-full md:w-auto bg-black md:bg-transparent p-4 md:p-0`}>
+                    <Link to="/" className="text-xl block md:inline">Home</Link>
+                    <button onClick={scrollToAbout} className="text-xl block md:inline">About</button>
+                    <Link to="/cart" className="relative block md:inline">
                         Cart
-                        {cartCount > 0 ? <div className="rounded-full bg-yellow-400 text-white inline-flex justify-center items-center w-full absolute -top-3 -right-6">{cartCount}</div> : null}
+                        {cartCount > 0 && (
+                            <div className="absolute -top-2 -right-3 bg-yellow-400 text-white rounded-full text-sm px-2">{cartCount}</div>
+                        )}
                     </Link>
-                    {
-                        isLoggedIn ?
-                            (<Button onClick={handleLogout}>Log Out</Button>) :
-                            (
-                                <>
-                                    <Link to="/login">Login</Link>
-                                    <Link to="/register">Sign Up</Link>
-                                </>
-                            )
-                    }
+                    {isLoggedIn ? (
+                        <Button onClick={handleLogout}>Log Out</Button>
+                    ) : (
+                        <>
+                            <Link to="/login" className="block md:inline">Login</Link>
+                            <Link to="/register" className="block md:inline">Sign Up</Link>
+                        </>
+                    )}
                 </div>
             </div>
         </nav>
